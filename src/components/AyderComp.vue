da5ce1f96ed8b3f133b40b2051cd5e6512e5bffb
Ayder <muzdabaev252@gmail.com>
2025-02-06 12:35:47 +0300

    Task 5#1

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -4,14 +4,17 @@
 	return {
 		name: 'john',
 		surn: 'smit',
+        attr: 'img.png',
+        text: 'page',
+		href: 'page.html',
 	}
-}
-		
+}	
 	}
 </script>
 <template>
 	<div>
         <p>{{ name }}</p>
         <p>{{ surn }}</p>
+        <a :href="page.html">page</a>
 	</div>
 </template>
