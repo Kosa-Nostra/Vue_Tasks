47ee07cb2766c5311f52e5f3d98927da4297808d
Ayder <muzdabaev252@gmail.com>
2025-02-06 21:44:49 +0300

    Task 14#1

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-    <button @click="showSquare(2)">Показать квадрат числа 2</button>
-    <button @click="showSquare(3)">Показать квадрат числа 3</button>
+    <p>{{ text }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    showSquare(number) {
-      const square = number ** 2;
-      alert(`Квадрат числа ${number} равен ${square}`);
-    }
+  data() {
+    return {
+      text: 'Пример текста'
+    };
   }
 }
 </script>
