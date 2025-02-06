e6aafda571f65f3f13a22bf2e9642969c7b668e8
Ayder <muzdabaev252@gmail.com>
2025-02-06 19:30:56 +0300

    Task 12#2

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -1,5 +1,10 @@
 <script>
 export default {
+  data() {
+    return {
+      currentDay: "",
+    };
+  },
   methods: {
     getWeekday(num) {
       const days = [
@@ -7,12 +12,20 @@ export default {
         "Среда", "Четверг", "Пятница", "Суббота"
       ];
       return days[num % 7] || "Некорректное число";
+    },
+    showCurrentDay() {
+      const today = new Date().getDay(); // Получаем номер дня (0 - Воскресенье, 6 - Суббота)
+      this.currentDay = this.getWeekday(today);
     }
+  },
+  mounted() {
+    this.showCurrentDay();
   }
-}
+};
 </script>
+
 <template>
-	<div>
-	  <p>{{ getWeekday(3) }}</p> 
-	</div>
-  </template>
+  <div>
+    <p>Сегодня: {{ currentDay }}</p>
+  </div>
+</template>
