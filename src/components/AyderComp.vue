289a9a2cfe521b52a0faa7925ba0208a6d3be194
Ayder <muzdabaev252@gmail.com>
2025-02-07 18:13:47 +0300

    Task 34

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -1,20 +1,19 @@
 <template>
-  <div>
-    <p v-for="(item, index) in positiveItems" :key="index">{{ item }}</p>
-  </div>
+  <ul>
+    <li v-for="product in products" :key="product.id">{{ product.name }}</li>
+  </ul>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      items: [1, -2, 3, -4, 5],
+      products: [
+        { id: 1, name: 'product1' },
+        { id: 2, name: 'product2' },
+        { id: 3, name: 'product3' },
+      ],
     };
-  },
-  computed: {
-    positiveItems() {
-      return this.items.filter(item => item > 0);
-    },
   },
 };
 </script>
