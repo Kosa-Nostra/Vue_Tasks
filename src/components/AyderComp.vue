108017532008f5a12d9e69e2e35f3447a49e6257
Ayder <muzdabaev252@gmail.com>
2025-02-07 17:52:03 +0300

    Task 17#1

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <button @click.once="handleClick">Нажмите меня</button>
+    <p v-if="visible">Первый абзац отображается, когда visible = true.</p>
+    <p v-else>Второй абзац отображается, когда visible = false.</p>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    handleClick() {
-      alert('Кнопка нажата!');
-    }
+  data() {
+    return {
+      visible: true // Изначальное значение свойства visible
+    };
   }
 }
 </script>
