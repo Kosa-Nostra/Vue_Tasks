c1424137cb5ff9d6599aa390c5e8f89ffcf41ed2
Ayder <muzdabaev252@gmail.com>
2025-02-07 17:53:14 +0300

    Task 18

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -1,15 +1,12 @@
 <template>
-  <div>
-    <p v-if="visible">Первый абзац отображается, когда visible = true.</p>
-    <p v-else>Второй абзац отображается, когда visible = false.</p>
-  </div>
+  <p v-if="!hidden">text</p>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      visible: true // Изначальное значение свойства visible
+      hidden: true // Изначальное значение свойства hidden
     };
   }
 }
