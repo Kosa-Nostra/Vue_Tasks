f99ac54bd402d2bbcc2a875cc83eade937996765
Ayder <muzdabaev252@gmail.com>
2025-02-07 18:12:03 +0300

    Task 32

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -1,27 +1,14 @@
 <template>
-  <table>
-    <tr>
-      <th>Название</th>
-      <th>Цена</th>
-      <th>Количество</th>
-    </tr>
-    <tr v-for="(product, index) in products" :key="index">
-      <td>{{ product.name }}</td>
-      <td>{{ product.price }}</td>
-      <td>{{ product.quantity }}</td>
-    </tr>
-  </table>
+  <div>
+    <p v-for="number in numbers" :key="number">{{ number }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      products: [
-        { name: 'product1', price: 100, quantity: 5 },
-        { name: 'product2', price: 200, quantity: 4 },
-        { name: 'product3', price: 300, quantity: 3 },
-      ],
+      numbers: Array.from({ length: 30 }, (_, index) => index + 1),
     };
   },
 };
