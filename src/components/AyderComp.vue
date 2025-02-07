7ca1619cce17cb48bc629b8b2088ae09636bdcc4
Ayder <muzdabaev252@gmail.com>
2025-02-07 21:13:30 +0300

    Task 36

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -1,28 +1,10 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="(item, index) in reversedItems" :key="index">{{ item }}</li>
-    </ul>
-    <button @click="reverseItems">Перевернуть список</button>
-  </div>
+  <p>
+    TEST
+  </p>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      items: ['Элемент 1', 'Элемент 2', 'Элемент 3'],
-    };
-  },
-  computed: {
-    reversedItems() {
-      return [...this.items].reverse();
-    },
-  },
-  methods: {
-    reverseItems() {
-      this.items = this.reversedItems;
-    },
-  },
-};
+
 </script>
+<style> p{color: greenyellow;}</style>
