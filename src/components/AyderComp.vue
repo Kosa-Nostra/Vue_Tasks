c5fe78931584e2e31fd394409c87c0835935ceb4
Ayder <muzdabaev252@gmail.com>
2025-02-08 20:02:08 +0300

    Task 58

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -38,24 +38,9 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      users: [
-        { id: 1, name: 'name1', salary: 100, age: 30, isEditing: false },
-        { id: 2, name: 'name2', salary: 200, age: 40, isEditing: false },
-        { id: 3, name: 'name3', salary: 300, age: 50, isEditing: false },
-      ]
-    };
-  },
-  methods: {
-    removeUser(index) {
-      this.users.splice(index, 1); // Удаляем пользователя по индексу
-    },
-    toggleEdit(index) {
-      const user = this.users[index];
-      user.isEditing = !user.isEditing; // Переключаем режим редактирования
-    }
-  }
-};
+	import EmployeeComp from './components/EmployeeComp.vue'
+	
+	export default {
+		
+	}
 </script>
