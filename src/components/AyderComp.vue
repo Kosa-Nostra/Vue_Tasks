1c3e8762b732bc08ae37fb4d33cb985862fb46ac
Ayder <muzdabaev252@gmail.com>
2025-02-08 19:57:27 +0300

    Task 56

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -13,10 +13,24 @@
       <tbody>
         <tr v-for="(user, index) in users" :key="user.id">
           <td>{{ user.id }}</td>
-          <td>{{ user.name }}</td>
-          <td>{{ user.salary }}</td>
-          <td>{{ user.age }}</td>
-          <td><a href="#" @click="removeUser(index)">Удалить</a></td> <!-- Ссылка для удаления -->
+          <td>
+            <input v-if="user.isEditing" v-model="user.name" />
+            <span v-else>{{ user.name }}</span>
+          </td>
+          <td>
+            <input v-if="user.isEditing" v-model="user.salary" />
+            <span v-else>{{ user.salary }}</span>
+          </td>
+          <td>
+            <input v-if="user.isEditing" v-model="user.age" />
+            <span v-else>{{ user.age }}</span>
+          </td>
+          <td>
+            <a href="#" @click="removeUser(index)">Удалить</a>
+            <a href="#" @click="toggleEdit(index)">
+              {{ user.isEditing ? 'Сохранить' : 'Редактировать' }}
+            </a>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -28,15 +42,19 @@ export default {
   data() {
     return {
       users: [
-        { id: 1, name: 'name1', salary: 100, age: 30 },
-        { id: 2, name: 'name2', salary: 200, age: 40 },
-        { id: 3, name: 'name3', salary: 300, age: 50 },
+        { id: 1, name: 'name1', salary: 100, age: 30, isEditing: false },
+        { id: 2, name: 'name2', salary: 200, age: 40, isEditing: false },
+        { id: 3, name: 'name3', salary: 300, age: 50, isEditing: false },
       ]
     };
   },
   methods: {
     removeUser(index) {
       this.users.splice(index, 1); // Удаляем пользователя по индексу
+    },
+    toggleEdit(index) {
+      const user = this.users[index];
+      user.isEditing = !user.isEditing; // Переключаем режим редактирования
     }
   }
 };
