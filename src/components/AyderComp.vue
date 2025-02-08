08e13a8fc95729b4b27dd02dfd8b9328b0191339
Ayder <muzdabaev252@gmail.com>
2025-02-08 19:40:02 +0300

    Task 45#2

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <textarea v-model="text" placeholder="Введите текст"></textarea>
-    <p>{{ text }}</p>
+    <button @click="splitText">Разбить на слова</button>
+    <ul v-if="words.length">
+      <li v-for="(word, index) in words" :key="index">{{ word }}</li>
+    </ul>
   </div>
 </template>
 
@@ -9,17 +12,22 @@
 export default {
   data() {
     return {
-      text: ""
+      text: "",
+      words: []
     };
+  },
+  methods: {
+    splitText() {
+      this.words = this.text.trim().split(/\s+/).filter(word => word);
+    }
   }
 };
 </script>
 
 <style scoped>
-textarea {
+textarea, button {
   display: block;
   width: 100%;
-  height: 100px;
   margin-bottom: 10px;
   padding: 5px;
   resize: vertical;
