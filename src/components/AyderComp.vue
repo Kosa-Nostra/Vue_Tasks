bb77735d442ce449e2f49bb06bfe4bce69f0ad5e
Ayder <muzdabaev252@gmail.com>
2025-02-08 19:47:06 +0300

    Task 50#2

--- src/components/AyderComp.vue
+++ src/components/AyderComp.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
-    <h3>Выберите день недели:</h3>
+    <h3>Выберите день, месяц и год:</h3>
+
     <select v-model="selectedDay">
-      <option value="Понедельник">Понедельник</option>
-      <option value="Вторник">Вторник</option>
-      <option value="Среда">Среда</option>
-      <option value="Четверг">Четверг</option>
-      <option value="Пятница">Пятница</option>
-      <option value="Суббота">Суббота</option>
-      <option value="Воскресенье">Воскресенье</option>
+      <option disabled value="">День</option>
+      <option v-for="day in days" :key="day" :value="day">{{ day }}</option>
     </select>
 
-    <div v-if="selectedDay">
-      <p>Вы выбрали: {{ selectedDay }}</p>
+    <select v-model="selectedMonth">
+      <option disabled value="">Месяц</option>
+      <option v-for="(month, index) in months" :key="index" :value="index + 1">{{ month }}</option>
+    </select>
+
+    <select v-model="selectedYear">
+      <option disabled value="">Год</option>
+      <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
+    </select>
+
+    <div v-if="selectedDay && selectedMonth && selectedYear">
+      <p>Вы выбрали: {{ selectedDay }} {{ months[selectedMonth - 1] }} {{ selectedYear }}</p>
     </div>
   </div>
 </template>
@@ -21,7 +27,15 @@
 export default {
   data() {
     return {
-      selectedDay: "" // Переменная для хранения выбранного дня недели
+      selectedDay: "", // Выбранный день
+      selectedMonth: "", // Выбранный месяц
+      selectedYear: "", // Выбранный год
+      days: Array.from({ length: 31 }, (_, i) => i + 1), // Массив дней от 1 до 31
+      months: [
+        "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+        "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+      ], // Массив месяцев
+      years: Array.from({ length: 101 }, (_, i) => 1900 + i) // Массив годов с 1900 по 2000
     };
   }
 };
